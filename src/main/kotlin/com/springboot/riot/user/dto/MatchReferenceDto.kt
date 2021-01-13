54eb21d4f7fd6c1434d074c4aa850654fa82d2ac
or50611 <korean_23@naver.com>
2021-01-13 11:39:48 +0900

    update commit

--- src/main/kotlin/com/springboot/riot/user/dto/MatchReferenceDto.kt
+++ src/main/kotlin/com/springboot/riot/user/dto/MatchReferenceDto.kt
@@ -3,11 +3,11 @@ package com.springboot.riot.user.dto
 data class MatchReferenceDto (
 	var gameId: Long = 0L,
 	var role: String? = null,
-	var season: Int? = 0,
+	var season: Int = 0,
 	var platformId: String? = null,
-	var champion: Int? = 0,
-	var queue: Int? = 0,
+	var champion: Int = 0,
+	var queue: Int = 0,
 	var lane: String? = null,
-	var timestamp: Long? = 0L
+	var timestamp: Long = 0L
 	
 )
